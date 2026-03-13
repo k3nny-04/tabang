3fba009dbc18094f571586562b3513667ec5aebe
KenAlwyn <ksvalenciano1@up.edu.ph>
2026-03-13 15:59:39 +0800

    chore: env changes

--- client/src/firebase-config.js
+++ client/src/firebase-config.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBM81FBSm0Y91HER_2iUEUq6Wwyjh5xsVU",
-  authDomain: "sp-app-a5722.firebaseapp.com",
-  projectId: "sp-app-a5722",
-  storageBucket: "sp-app-a5722.firebasestorage.app",
-  messagingSenderId: "121507250889",
-  appId: "1:121507250889:web:29546eac5375f327e54318",
-  measurementId: "G-4HPVMLD6Y3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
