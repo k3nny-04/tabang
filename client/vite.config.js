e947d7e2fff01ac65e89b40525d26909d152cccc
KenAlwyn <ksvalenciano1@up.edu.ph>
2026-03-14 15:57:30 +0800

    fix: pwa max size limit

--- client/vite.config.js
+++ client/vite.config.js
@@ -30,6 +30,9 @@ export default defineConfig({
             type: 'image/png'
           }
         ]
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5000000, 
       }
     })
   ],
