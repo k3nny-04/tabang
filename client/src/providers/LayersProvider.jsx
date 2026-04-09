db2be26229ce56aaed89d93c91b419757bf519f1
KenAlwyn <ksvalenciano1@up.edu.ph>
2026-04-09 23:00:03 +0800

    chore: added hazard maps

--- client/src/providers/LayersProvider.jsx
+++ client/src/providers/LayersProvider.jsx
@@ -4,20 +4,42 @@ const LayersContext = createContext(null);
 
 export const LayersProvider = ({ children }) => {
   const [activeLayers, setActiveLayers] = useState({
+    // Markers (Multiple allowed)
     evacShelters: true,
+    incidentReports: false,
+    responseTeams: false,
+    // Hazards (Mutually exclusive)
+    floodMap: false,
+    landslide: false,
+    stormSurge: false,
   });
 
   const toggleLayer = (layer) => {
-    setActiveLayers((prev) => ({
-      ...prev,
-      [layer]: !prev[layer],
-    }));
+    setActiveLayers((prev) => {
+      const hazards = ["floodMap", "landslide", "stormSurge"];
+      const isHazard = hazards.includes(layer);
+
+      if (isHazard) {
+        const turningOn = !prev[layer];
+        if (turningOn) {
+          return {
+            ...prev,
+            floodMap: layer === "floodMap",
+            landslide: layer === "landslide",
+            stormSurge: layer === "stormSurge",
+          };
+        }
+      }
+
+      return {
+        ...prev,
+        [layer]: !prev[layer],
+      };
+    });
   };
 
   return (
-    <LayersContext.Provider
-      value={{ activeLayers, toggleLayer }}
-    >
+    <LayersContext.Provider value={{ activeLayers, toggleLayer }}>
       {children}
     </LayersContext.Provider>
   );
