43bd93b88f80c17a70f20cd9f4d24136d8fc597b
KenAlwyn <ksvalenciano1@up.edu.ph>
2026-04-16 13:04:10 +0800

    chore: splash logo changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -22,12 +22,14 @@ export default defineConfig({
           {
             src: 'logo-black-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any'
           },
           {
-            src: 'logo-black-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: 'logo-black-1024x1024.png',
+            sizes: '1024x1024',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       },
